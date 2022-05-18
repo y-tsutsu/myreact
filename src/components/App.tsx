00e2882c76113ec60f518f228e42b5bda4e580b2
tsutsu <tsutsu0928@gmail.com>
2022-05-18 13:36:33 +0900

    リファクタリング

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,41 +1,27 @@
-import { ChangeEvent, useState, FC } from "react";
+import { ChangeEvent, useState, FC, useCallback } from "react";
 import styled from "styled-components";
+import { MemoList } from "./MemoList";
+import { useMemoList } from "../hooks/useMemoList";
 
 export const App: FC = () => {
     const [text, setText] = useState<string>("");
-    const [memos, setMemos] = useState<string[]>([]);
+    const { memos, addTodo, deleteTodo } = useMemoList()
 
     const onChangeText = (e: ChangeEvent<HTMLInputElement>) => setText(e.target.value);
     const onClickedAdd = () => {
-        const newMemos = [...memos];
-        newMemos.push(text);
-        setMemos(newMemos);
+        addTodo(text);
         setText("");
     }
-    const onClickedDelete = (index: number) => {
-        const newMemos = [...memos];
-        newMemos.splice(index, 1);
-        setMemos(newMemos);
-    }
+    const onClickDelete = useCallback((index: number) => {
+        deleteTodo(index);
+    }, [deleteTodo]);
 
     return (
         <div>
             <h1>簡単メモアプリ</h1>
             <input type="text" value={text} onChange={onChangeText} />
             <SButton onClick={onClickedAdd}>追加</SButton>
-            <SContainer>
-                <p>メモ一覧</p>
-                <ul>
-                    {memos.map((memo, index) => (
-                        <li key={memo}>
-                            <SMemoWrapper>
-                                <p>{memo}</p>
-                                <SButton onClick={() => onClickedDelete(index)}>削除</SButton>
-                            </SMemoWrapper>
-                        </li>
-                    ))}
-                </ul>
-            </SContainer>
+            <MemoList memos={memos} onClickDelete={onClickDelete}></MemoList>
         </div>
     );
 };
@@ -43,12 +29,3 @@ export const App: FC = () => {
 const SButton = styled.button`
     margin-left: 16px;
 `;
-const SContainer = styled.div`
-    border: solid 1px #ccc;
-    padding: 16px;
-    margin: 8px;
-`
-const SMemoWrapper = styled.div`
-    display: flex;
-    align-items: center;
-`
